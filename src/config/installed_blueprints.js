c7cc5c12c2052e7e8e616e69feb75b4f33ad29c3
Crowdbotics <team@crowdbotics.com>
2020-11-25 23:41:56 +0000

    add package blueprint-react-native-message-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Messaging7178790', human_name: 'Messaging 7', access_route: 'Messaging7178790', icon: 'comments'},
 { name: 'Camera9178788', human_name: 'Camera 9', access_route: 'Camera9178788', icon: 'camera'},
 { name: 'Tutorial10178787', human_name: 'Tutorial 10', access_route: 'Tutorial10178787', icon: 'question-circle'},
 
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Messaging7178790Navigator from '../features/Messaging7178790/navigator';
 import Camera9178788Navigator from '../features/Camera9178788/navigator';
 import Tutorial10178787Navigator from '../features/Tutorial10178787/navigator';
 
@@ -15,6 +16,7 @@ import Tutorial10178787Navigator from '../features/Tutorial10178787/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Messaging7178790: { screen: Messaging7178790Navigator },
 Camera9178788: { screen: Camera9178788Navigator },
 Tutorial10178787: { screen: Tutorial10178787Navigator },
 
